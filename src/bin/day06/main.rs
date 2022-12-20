fc516699cca96f4bea2441efe418ca4da85a9031
Lukas Stephan <lukas.stephan@mailbox.org>
2022-12-20 17:36:29 +0100

    Some refactorings in day06

--- src/bin/day06/main.rs
+++ src/bin/day06/main.rs
@@ -1,28 +1,28 @@
 use itertools::Itertools;
 
-fn parse_input(input : &str, window_size : usize) -> Option<usize> {
-    Some(window_size + input.chars()
+fn find_marker(datastream : &str, marker_size : usize) -> Option<usize> {
+    Some(marker_size + datastream.chars()
         .collect::<Vec<char>>()
-        .windows(window_size)
-        .position(|slice| slice.iter().sorted().dedup().count() == slice.iter().count())?)
+        .windows(marker_size)
+        .position(|window| window.iter().sorted().dedup().count() == window.iter().count())?)
 }
 
 fn main() {
     let input = include_str!("input");
-    println!("Output 1: {}\nOutput 2: {}", parse_input(input,4).unwrap(), parse_input(input,14).unwrap());
+    println!("Output 1: {}\nOutput 2: {}", find_marker(input,4).unwrap(), find_marker(input,14).unwrap());
 }
 
 
 #[test]
 fn test_parser() {
-    assert_eq!(parse_input("bvwbjplbgvbhsrlpgdmjqwftvncz",4), Some(5));
-    assert_eq!(parse_input("nppdvjthqldpwncqszvftbrmjlhg",4), Some(6));
-    assert_eq!(parse_input("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg",4), Some(10));
-    assert_eq!(parse_input("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw",4), Some(11));
+    assert_eq!(find_marker("bvwbjplbgvbhsrlpgdmjqwftvncz",4), Some(5));
+    assert_eq!(find_marker("nppdvjthqldpwncqszvftbrmjlhg",4), Some(6));
+    assert_eq!(find_marker("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg",4), Some(10));
+    assert_eq!(find_marker("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw",4), Some(11));
 
-    assert_eq!(parse_input("mjqjpqmgbljsphdztnvjfqwrcgsmlb",14), Some(19));
-    assert_eq!(parse_input("bvwbjplbgvbhsrlpgdmjqwftvncz",14), Some(23));
-    assert_eq!(parse_input("nppdvjthqldpwncqszvftbrmjlhg",14), Some(23));
-    assert_eq!(parse_input("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg",14), Some(29));
-    assert_eq!(parse_input("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw",14), Some(26));
+    assert_eq!(find_marker("mjqjpqmgbljsphdztnvjfqwrcgsmlb",14), Some(19));
+    assert_eq!(find_marker("bvwbjplbgvbhsrlpgdmjqwftvncz",14), Some(23));
+    assert_eq!(find_marker("nppdvjthqldpwncqszvftbrmjlhg",14), Some(23));
+    assert_eq!(find_marker("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg",14), Some(29));
+    assert_eq!(find_marker("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw",14), Some(26));
 }
