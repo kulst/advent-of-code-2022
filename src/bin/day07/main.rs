7fca19d184f97f03b6b5693f49ce316afef4bc1b
Lukas Stephan <lukas.stephan@mailbox.org>
2022-12-22 01:39:01 +0100

    Added partly solution for day07

---
+++ src/bin/day07/main.rs
@@ -0,0 +1,18 @@
+use std::collections::HashMap;
+use std::path::{Path, PathBuf};
+use thiserror::;
+
+struct Data {
+    size : usize,
+    is_folder : bool,
+}
+
+fn update_folder_sizes(file_size : usize, file_path : &Path, map : &mut HashMap<PathBuf, Data>) {
+    while let Some(parent) = file_path.parent() {
+        map.get_mut(parent).unwrap().size += file_size;
+    }
+}
+
+fn parse_input(s : &str) -> HashMap<PathBuf, Data> {
+
+}
